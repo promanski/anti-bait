b7f5f75eb54356ebf855ba7f7ffbc6d5d8ca2b2e
promanski <romanskipiotr@hotmail.com>
2023-02-27 18:37:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-function selectText(text){
-  var parts = text.split("/");
-  parts = parts[(parts.length)-2];
-  parts = parts.replace(/-/g, ' ');
-  return parts.charAt(0).toUpperCase() + parts.slice(1);
+function extractTextFromUrl(url) {
+  // Split the url by "/" and get the penultimate element
+  let segments = url.split("/");
+  let lastSegment = segments[segments.length - 2];
+  // Replace "-" with " " and capitalize the first letter
+  let text = lastSegment.replace(/-/g, " ").replace(/^\w/, c => c.toUpperCase());
+  // Return the text
+  return text;
 }
 
-function replaceContent(){
-var links = $('a[href^="http://"]');
+// Get all links on the webpage using document.links property
+let links = document.links;
 
-  for (var index=0; index<links.length; index++){
-      var trueText = selectText(links[index].href);
-      var fakeText = getTextFromTitle(links[index]).text();
-      getTextFromTitle(links[index]).text(trueText);
+// Loop through each link and replace its last child's content with its extracted text
+for (let link of links) {
+  // Get the href attribute of the link
+  let cls = link.className;
+  // Check if the class starts with BigCard using startsWith method
+  if (!cls.startsWith("BigCard") || !cls.startsWith('Menu')) {
+  let href = link.href;
+  // Get the extracted text from the href using extractTextFromUrl function
+  let text = extractTextFromUrl(href);
+  // Get the last child element of the link using lastElementChild property
+  let lastChild = link.lastElementChild;
+  // Replace its content with text using innerHTML property
+    let grandChild = lastChild.firstElementChild;
+// If it exists, replace its content with text using innerHTML property
+    if (grandChild) {
+      grandChild.textContent = text;
+    }
+    else if (lastChild && !grandChild){
+      lastChild.textContent = text;
+    }
   }
-}
-
-function getTextFromTitle(el) {
-    return $(el).find('.title');
-}
-if (window.location.href.indexOf("onet.pl" >-1)){
-replaceContent();
 }
